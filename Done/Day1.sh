f246f62fbb492d29bc424fb4607cc416d9be4221
Siddharth Gupta <sidhharth.gupta@mygurukulam.org>
2020-12-27 22:47:13 +0530

    Removing Code

--- Done/Day1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This bash prints the hard coded text
-
-SKILL="learn"
-echo " ${SKILL}ing is a fun task"
-SKILL="play"
-echo " ${SKILL}ing is a fun task."
--- Done/HelloWorld.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#This is a simple bash script that prints the Hello World
-echo "Hello World"
